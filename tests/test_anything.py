a982565d0435a7c7a8bde000497a58d335e8ec88
Mehrshad <meromero662@gmail.com>
2023-11-02 13:40:26 +0100

    !

---
+++ tests/test_anything.py
@@ -0,0 +1,18 @@
+from your_flask_app import app
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_anything_view(client):
+    response = client.get('/anything')
+    assert response.status_code == 200
+    assert b"Random Activity" in response.data
+    assert b"Here's a random activity you can try:" in response.data
+    # Här kan du lägga till fler assertions för att kontrollera innehållet på sidan
+
+    # Exempel: Kontrollera att knappen finns
+    assert b"Next Random Activity" in response.data
